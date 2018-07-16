a07d91894d41ec8e8358aff0948c4056182f3f8f
Anton Chernov <mechernov@gmail.com>
2018-07-16 19:55:34 +0200

    Refactored MKL / MKLML / MKLDNN cmake

--- cmake/DownloadMKLML.cmake
+++ cmake/DownloadMKLML.cmake
@@ -15,30 +15,39 @@
 # specific language governing permissions and limitations
 # under the License.
 
-#this file download mklml
+# This script will download mkldnn with dependencies such as intel omp and mklml
 
-message(STATUS "download mklml")
+message(STATUS "downloading mkldnn...")
+
 if(MSVC)
   set(MKL_NAME "mklml_win_2018.0.3.20180406")
   file(DOWNLOAD "https://github.com/intel/mkl-dnn/releases/download/v0.14/${MKL_NAME}.zip" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.zip" EXPECTED_MD5 "8DD73E7D3F19F004551809824C4E8970" SHOW_PROGRESS)
   file(DOWNLOAD "https://github.com/apache/incubator-mxnet/releases/download/utils/7z.exe" "${CMAKE_CURRENT_BINARY_DIR}/mklml/7z2.exe" EXPECTED_MD5 "E1CF766CF358F368EC97662D06EA5A4C" SHOW_PROGRESS)
-  
+
   execute_process(COMMAND "${CMAKE_CURRENT_BINARY_DIR}/mklml/7z2.exe" "-o${CMAKE_CURRENT_BINARY_DIR}/mklml/" "-y")
   execute_process(COMMAND "${CMAKE_CURRENT_BINARY_DIR}/mklml/7z.exe" "x" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.zip" "-o${CMAKE_CURRENT_BINARY_DIR}/mklml/" "-y")
-  set(MKLROOT "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}")
-  include_directories(${MKLROOT}/include)
-  file(COPY ${MKLROOT}/lib/libiomp5md.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  file(COPY ${MKLROOT}/lib/mklml.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  set(MKLDNN_ROOT "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}")
+
+  file(COPY ${MKLDNN_ROOT}/lib/libiomp5md.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  file(COPY ${MKLDNN_ROOT}/lib/mklml.dll DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
   file(COPY ${CMAKE_SOURCE_DIR}/3rdparty/mkldnn/config_template.vcxproj.user DESTINATION ${CMAKE_SOURCE_DIR})
 elseif(UNIX)
   set(MKL_NAME "mklml_lnx_2018.0.3.20180406")
   file(DOWNLOAD "https://github.com/intel/mkl-dnn/releases/download/v0.14/${MKL_NAME}.tgz" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.tgz" EXPECTED_MD5 "DAF7EFC3C1C0036B447213004467A8AE" SHOW_PROGRESS)
   execute_process(COMMAND "tar" "-xzf" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.tgz" "-C" "${CMAKE_CURRENT_BINARY_DIR}/mklml/")
-  set(MKLROOT "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}")
-  include_directories(${MKLROOT}/include)
-  file(COPY ${MKLROOT}/lib/libiomp5.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  file(COPY ${MKLROOT}/lib/libmklml_gnu.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  file(COPY ${MKLROOT}/lib/libmklml_intel.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  set(MKLDNN_ROOT "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}")
+
+  file(COPY ${MKLDNN_ROOT}/lib/libiomp5.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  file(COPY ${MKLDNN_ROOT}/lib/libmklml_gnu.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  file(COPY ${MKLDNN_ROOT}/lib/libmklml_intel.so DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+elseif(APPLE)
+  set(MKL_NAME "mklml_mac_2018.0.3.20180406.tgz")
+  file(DOWNLOAD "https://github.com/intel/mkl-dnn/releases/download/v0.14/${MKL_NAME}.tgz" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.tgz" EXPECTED_MD5 "23a6f7fd04fb1fa6de0d52a2ec5a2a14" SHOW_PROGRESS)
+  execute_process(COMMAND "tar" "-xzf" "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}.tgz" "-C" "${CMAKE_CURRENT_BINARY_DIR}/mklml/")
+  set(MKLDNN_ROOT "${CMAKE_CURRENT_BINARY_DIR}/mklml/${MKL_NAME}")
+
+  file(COPY ${MKLDNN_ROOT}/lib/libiomp5.dylib DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
+  file(COPY ${MKLDNN_ROOT}/lib/libmklml.dylib DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
 else()
-  message(FATAL_ERROR "not support now")
+  message(FATAL_ERROR "Wrong platform")
 endif()
